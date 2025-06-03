816aef1c5b356fc014c41e4bc520d4a93079498f
EngHassan8 <hassannaana99@gmail.com>
2025-06-03 19:17:00 +0300

    Successful build: fixed import issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import About from './pages/about'
+import About from './pages/About'
 import Contact from './pages/Contact'
 import Project from './pages/Project'
 
